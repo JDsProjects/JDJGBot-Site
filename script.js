c731e194166331f6d776fd5cbd6370b9488866cd
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-03 17:38:02 -0500

    Add bg expander

---
+++ script.js
@@ -0,0 +1,7 @@
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+};
+ready(() => {
+  document.querySelector(".header").style.height = window.innerHeight + "px";
+});
