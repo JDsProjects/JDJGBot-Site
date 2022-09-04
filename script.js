357f0532722518c72c35d00c100b81a26656f8e9
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-04 15:55:00 -0500

    VH-mobile

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ ready(() => {
   document.querySelector(".header").style.height = window.innerHeight + "px";
 
 });
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
