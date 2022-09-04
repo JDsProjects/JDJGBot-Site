e226a2f2f32c6d34254bd80a68abb8d5936e649e
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-04 17:02:05 -0500

    Upgrade 404.html (#11)
    * Add slash permanent incase of /index.html path
    
    * Beta warning
    
    * Update script.js
    
    * Update script.js
    
    * Remove beta warning
    
    * 404 upgrade
    
    * !
    
    * Update 404.html
    
    * Update 404.html
    
    * Update 404.html
    
    * Update 404.html
    
    * Update 404.html
    
    * Update 404.html
    
    * Update 404.html
    
    * Update 404.html
    
    * vH on mobile
    
    * VH-mobile
    
    * Add fallback

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ var ready = (callback) => {
 };
 ready(() => {
   document.querySelector(".header").style.height = window.innerHeight + "px";
+
 });
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
