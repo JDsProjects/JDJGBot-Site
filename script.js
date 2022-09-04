4c91c0ad503ee6bfeae2697901a5cbeda3add70d
JDJG Inc. Official <ghostzap123@gmail.com>
2022-09-04 13:51:23 -0400

    Revert "[Tested] Website upgrade"

--- script.js
+++
@@ -1,7 +0,0 @@
-var ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
-};
-ready(() => {
-  document.querySelector(".header").style.height = window.innerHeight + "px";
-});
