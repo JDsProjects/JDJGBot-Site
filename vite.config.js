d3f349afaf87d41faa03c3ce2edbc1d6a8f26e24
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2023-10-28 04:40:03 +0100

    Prepare for CF Pages (#15)
    * Add bundler
    
    * Prepare for CF Pages

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    clearScreen: false,
+    server: {
+        host: true,
+        port: 3000,
+    },
+});
